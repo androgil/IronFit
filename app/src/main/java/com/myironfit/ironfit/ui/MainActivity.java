28904c391e849ced422405c73ef9b3a83ee1a774
androgil <ericsgilmore@gmail.com>
2020-01-19 16:38:44 -0500

    Drawable header image resource change

---
+++ app/src/main/java/com/myironfit/ironfit/ui/MainActivity.java
@@ -0,0 +1,173 @@
+package com.myironfit.ironfit.ui;
+
+import android.os.Bundle;
+
+import com.google.android.material.navigation.NavigationView;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.myironfit.ironfit.MyCalendar;
+import com.myironfit.ironfit.R;
+import com.myironfit.ironfit.adapters.WeekdayRecyclerViewAdapter;
+import com.myironfit.ironfit.data.myCalendarData;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String TAG = "MainActivity";
+
+    private List<MyCalendar> calendarList = new ArrayList<>();
+    private WeekdayRecyclerViewAdapter mWkdAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar mainToolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(mainToolbar);
+
+        //Setting current month day year date text view
+        String date_mdy = new SimpleDateFormat("MMM dd, YYYY", Locale.getDefault()).format(new Date());
+        TextView date = findViewById(R.id.dateTextView);
+        date.setText(date_mdy);
+
+        //Setting current weekday text view
+        String date_wd = new SimpleDateFormat("EEEE", Locale.getDefault()).format(new Date());
+        TextView weekday = findViewById(R.id.weekdayTextView);
+        weekday.setText(date_wd);
+
+        mWkdAdapter = new WeekdayRecyclerViewAdapter(calendarList, this);
+
+        /**
+         * Sidebar or Drawer
+         */
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, mainToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        initWkdRecyclerView();
+    }
+
+    /**
+     * Horizontal weekday recycler view initialization
+     */
+
+    private void initWkdRecyclerView(){
+        Log.d(TAG, "initWkdRecyclerView: init recyclerview");
+
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        RecyclerView recyclerView = findViewById(R.id.weekdayRecycler);
+        recyclerView.setLayoutManager(layoutManager);
+        WeekdayRecyclerViewAdapter adapter = new WeekdayRecyclerViewAdapter(calendarList, this);
+
+        recyclerView.setAdapter(adapter);
+
+        prepareCalendarData();
+
+    }
+
+    /**
+     * Prepares sample data to provide data set to adapter
+     */
+    private void prepareCalendarData() {
+
+        // run a for loop for all the next 30 days of the current month starting today
+        // initialize mycalendarData and get Instance
+        // getnext to get next set of date etc.. which can be used to populate MyCalendarList in for loop
+
+        myCalendarData m_calendar = new myCalendarData(-2);
+
+        for ( int i=0; i <30; i++) {
+
+            MyCalendar calendar = new MyCalendar(m_calendar.getWeekDay(), String.valueOf(m_calendar.getDay()), String.valueOf(m_calendar.getMonth()), String.valueOf(m_calendar.getYear()),i);
+            m_calendar.getNextWeekDay(1);
+
+            calendarList.add(calendar);
+
+        }
+
+        // notify adapter about data set changes
+        // so that it will render the list with new data
+
+        mWkdAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
